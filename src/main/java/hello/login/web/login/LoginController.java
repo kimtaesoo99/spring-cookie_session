c81f78b81a2e57d506c15dca096314f8ff387c8b
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-06 16:15:55 +0900

    feat: 로그아웃 추가

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -25,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response){
         if (bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -38,8 +40,22 @@ public class LoginController {
 
         //로그인 성공 처리
 
+        //쿠키에 시간정보를 주지 않으면 세션 쿠기(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
     }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response,"memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response,String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
 
 }
